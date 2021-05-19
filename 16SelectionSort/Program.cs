2941b7902bb440f0f85177b4bf5e8ed143a08c61
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-05-19 22:05:26 +0300

    ty

---
+++ 16SelectionSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _16SelectionSort
+{
+    class Program
+    {
+        private static void SelectionSort(int[] arr)
+        {
+            int aux;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int k = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                    if (arr[j] < arr[k])
+                        k = j;
+
+                aux = arr[k];
+                arr[k] = arr[i];
+                arr[i] = aux;
+            }
+        }
+        static void View(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i]} ");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 6, 2, 3, 4, 1, 2, 8, 7, 9 };
+            View(arr);
+            SelectionSort(arr);
+            View(arr);
+        }
+    }
+}

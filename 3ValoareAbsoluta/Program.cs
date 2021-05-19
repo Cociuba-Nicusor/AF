f6b6a988bdd08b44d279084ab4fa2559f155f629
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-05-19 21:18:50 +0300

    ty

---
+++ 3ValoareAbsoluta/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _3ValoareAbsoluta
+{
+    class Program
+    {
+        static void Read(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        static void View(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i]} ");
+            Console.WriteLine();
+        }
+        static int f(int[] arr, int i, int j)
+        {
+            return (Math.Abs(arr[i] - arr[j]) + Math.Abs(i - j));
+        }
+
+        static int FindMax(int[] arr)
+        {
+            int n = arr.Length;
+            int max = 0;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = i + 1; j < n; j++)
+                {
+                    int res = f(arr, i, j);
+                    if (res > max)
+                        max = res;
+                }
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Read(arr);
+            View(arr);
+
+            int max = FindMax(arr);
+            Console.WriteLine($"Maximul este {max}");
+        }
+    }
+}

cc7ad5950d565ae6a40c73e8ddcb4d4c6f141bc2
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-05-19 21:52:26 +0300

    ty

---
+++ 9Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _9Fibonacci
+{
+      class Program
+      {
+            static int Fibonacci(int n)
+            {
+                if (n <= 1)
+                    return n;
+                else
+                    return Fibonacci(n - 1) + Fibonacci(n - 2);
+
+            }
+            static void Main(string[] args)
+            {
+                for (int i = 1; i < 10; i++)
+                Console.WriteLine(Fibonacci(i));
+                Console.ReadKey();
+            }
+      }
+}
+
+
